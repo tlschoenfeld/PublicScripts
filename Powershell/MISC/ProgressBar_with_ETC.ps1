f46284ec43e360d0799d3aa54f2c4c45c3c45e77
tlschoenfeld <62017477+tlschoenfeld@users.noreply.github.com>
2021-10-06 15:28:55 -0400

    Add files via upload

---
+++ Powershell/MISC/ProgressBar_with_ETC.ps1
@@ -0,0 +1,25 @@
+﻿# Data Source
+$Information = 1..1000000
+
+# Initialize Tracking
+$start = Get-Date
+$i = 0
+$total = $Information.Count
+
+# Loop
+foreach ($piece in $information) {
+	# Progress Tracking
+	$i++
+	$prct = [Math]::Round((($i / $total) * 100.0), 2)
+
+	$elapsed = (Get-Date) - $start
+	$totalTime = ($elapsed.TotalSeconds) / ($prct / 100.0)
+	$remain = $totalTime - $elapsed.TotalSeconds
+	$eta = (Get-Date).AddSeconds($remain)
+	
+	# Display
+	Write-Progress -Activity "What is going on (ETC $eta)" -Status "$prct %" -PercentComplete $prct
+	
+	# Operation
+
+}
